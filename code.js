5ba9fa1c9b7ee9045af6c0d278d82b90c7e8a1a4
izzyonez89 <36787601+izzyonez89@users.noreply.github.com>
2019-04-06 10:51:34 +0800

    Add files via upload

---
+++ code.js
@@ -0,0 +1,15 @@
+//declare variable
+var score;
+console.log(score);
+
+//declare a variable and initialize
+var livesLeft = 3;
+console.log(livesLeft);
+
+//reassign a valuw to our variable livesleft
+livesLeft = 2;
+console.log(livesLeft);
+
+//constants
+const firstname = 'John';
+
